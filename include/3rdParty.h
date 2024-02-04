165182c462a041d997b4e82b930ce5647ed37979
Alexander Wenzel <wraithone@outlook.de>
2024-02-04 12:49:31 +0100

    Removed 3rd Party Librarys
    SDL2 + ASSIMP

--- include/3rdParty.h
+++ include/3rdParty.h
@@ -1,11 +1,4 @@
 #pragma once
-// Simple Directmedia Layer
-#include <SDL.h>
-#include <SDL_main.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
-#include <SDL_net.h>
 
 // Assimp
 
